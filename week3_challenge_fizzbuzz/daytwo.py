ccf0058e12a5ebec42f488b3a06097ac1d5c275f
jennizalwango <jennyzalwango12@gmail.com>
2018-11-19 20:23:55 +0300

    refactoring my code

--- week3_challenge_fizzbuzz/daytwo.py
+++ week3_challenge_fizzbuzz/daytwo.py
@@ -1,9 +1,7 @@
 
 
 
-def fizzbuzz():
-    listA = [1,2,4,5,7.5, 12]
-    listb = [1,4,5,5,7.5,6]
+def fizzbuzz(listA ,listb):
     answer = sum(listA) + sum(listb)
     print(answer)
     if answer % 3 == 0 and answer % 5 == 0:
@@ -16,5 +14,7 @@ def fizzbuzz():
         return "sum of lists not divisible by 5 or 3"
 
 
-result = fizzbuzz()
+result = fizzbuzz(listA=[1,2,4,5,7.5,12],listb=[1,4,5,5,7.5,6])
 print(result)
+result2 = fizzbuzz(listA=[4,5,6,7,9,2],listb=[4,6,8,1,3,])
+print(result2)
